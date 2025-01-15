7dc1c4ea231a91c4cd8d108527a784cd755549a7
darylescode <darylescode@gmail.com>
2025-01-16 04:37:30 +0800

    Add selectedTodo state to TodoProvider for managing currently selected todo

--- client/src/context/todo/TodoProvider.tsx
+++ client/src/context/todo/TodoProvider.tsx
@@ -5,9 +5,12 @@ import { todoData } from "@/shared/constants/data/todoData";
 
 function TodoProvider({ children }: React.PropsWithChildren) {
   const [todo, setTodo] = useState<ITodo[]>([...todoData]);
+  const [selectedTodo, setSelectedTodo] = useState<ITodo | null>(null);
 
   return (
-    <TodoContext.Provider value={{ todo, setTodo }}>
+    <TodoContext.Provider
+      value={{ todo, setTodo, selectedTodo, setSelectedTodo }}
+    >
       {children}
     </TodoContext.Provider>
   );
